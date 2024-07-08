323ff9d1c4b0d83bcad5200c79d6a6289dfcbca8
Peter Hagman <peter.hagman@sedesign.se>
2024-07-08 10:01:52 +0200

    Inc slick slider js in project

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,6 +5,7 @@
 function seod_enqueue_files() {
 	$temp_dir = get_stylesheet_directory_uri();
 	wp_enqueue_style( 'main',  $temp_dir . '/css/main.min.css' );
-  wp_enqueue_script( 'main', $temp_dir . '/js/main.min.js', array('jquery' ), false, true);
+	wp_enqueue_script( 'slick', $temp_dir . '/js/slick.min.js', array('jquery' ), false, true);
+  	wp_enqueue_script( 'main', $temp_dir . '/js/main.min.js', array('jquery' ), false, true);
 }
 add_action( 'wp_enqueue_scripts', 'seod_enqueue_files' );
