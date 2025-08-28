2be6e0218f12e39f5d74cd14b931ba201263962b
Peter Hagman <peter.hagman@sedesign.se>
2025-08-28 09:18:12 +0200

    Edit sftp.json file

--- inc/setup.php
+++ inc/setup.php
@@ -109,6 +109,17 @@ function seod_divi_page_load(){ ?>
 <?php
 }
 
+
+/* Remove WP version from header and RSS
+================================================== */
+remove_action( 'wp_head', 'wp_generator' );
+
+/* Disable file editing in WordPress admin
+================================================== */
+if ( ! defined( 'DISALLOW_FILE_EDIT' ) ) {
+  define( 'DISALLOW_FILE_EDIT', true );
+}
+
 /* Restrict REST API user endpoint to admins only
 ================================================== */
 add_filter( 'rest_endpoints', function( $endpoints ) {
@@ -119,3 +130,13 @@ add_filter( 'rest_endpoints', function( $endpoints ) {
   }
   return $endpoints;
 });
+
+/* Enforce strong passwords for all users except admins
+================================================== */
+add_action( 'user_profile_update_errors', function( $errors, $update, $user ) {
+  if ( isset( $_POST['pass1'] ) && !empty( $_POST['pass1'] ) ) {
+      if ( strlen( $_POST['pass1'] ) < 12 ) {
+          $errors->add( 'weak_password', '<strong>ERROR</strong>: Please use a password with at least 12 characters.' );
+      }
+  }
+}, 10, 3 );
