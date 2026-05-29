69e6ac87206b2360a566ad86d3e920937915f599
Peter Hagman <peter.hagman@sedesign.se>
2026-05-29 12:50:16 +0200

    Added custom shortcode for reg. and translate of strings.

--- inc/functionality.php
+++ inc/functionality.php
@@ -31,6 +31,42 @@ function seod_shortcode_wp_menu( $atts ) {
 }
 add_shortcode( 'wp_menu', 'seod_shortcode_wp_menu' );
 
+/* Shortcode: Polylang manuell översättning (Användning: [pll_string name="unik_nyckel" default="Ursprungstext"])
+================================================== */
+
+// Registrera sparade strängar på init så Polylang hinner plocka upp dem
+function seod_register_pll_strings() {
+  if ( ! function_exists( 'pll_register_string' ) ) return;
+  $strings = get_option( 'seod_pll_strings', [] );
+  foreach ( $strings as $name => $default ) {
+    pll_register_string( $name, $default, 'Shortcode strängar' );
+  }
+}
+add_action( 'init', 'seod_register_pll_strings' );
+
+function seod_shortcode_pll_string( $atts ) {
+  $atts = shortcode_atts( [
+    'name'    => '',
+    'default' => '',
+  ], $atts, 'pll_string' );
+
+  if ( empty( $atts['name'] ) ) return '';
+
+  // Spara strängen i option så den registreras på init vid nästa laddning
+  $strings = get_option( 'seod_pll_strings', [] );
+  if ( ! array_key_exists( $atts['name'], $strings ) || $strings[ $atts['name'] ] !== $atts['default'] ) {
+    $strings[ $atts['name'] ] = $atts['default'];
+    update_option( 'seod_pll_strings', $strings );
+  }
+
+  if ( function_exists( 'pll__' ) ) {
+    return esc_html( pll__( $atts['default'] ) );
+  }
+
+  return esc_html( $atts['default'] );
+}
+add_shortcode( 'pll_string', 'seod_shortcode_pll_string' );
+
 /* Menu toogle element
 ================================================== */
 function add_expand_collapsable_placeholder( $items, $args ) {
