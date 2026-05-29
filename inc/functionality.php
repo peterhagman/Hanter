35f05ce534dc2a8c5df77da932382ab197a7afec
Peter Hagman <peter.hagman@sedesign.se>
2026-05-29 12:36:32 +0200

    Added support for custom WP-menus

--- inc/functionality.php
+++ inc/functionality.php
@@ -1,4 +1,36 @@
 <?php
+/* Registrera navigationsmenyer
+================================================== */
+register_nav_menus( [
+  'losningar' => __( 'Lösningar', 'hanter' ),
+  'support'   => __( 'Support', 'hanter' ),
+  'om-oss'    => __( 'Om oss', 'hanter' ),
+] );
+
+/* Shortcode: Rendera WP-meny baserat på menyns namn (Användning: [wp_menu name="Menynamn"] eller [wp_menu name="Menynamn" depth="2"])
+================================================== */
+function seod_shortcode_wp_menu( $atts ) {
+  $atts = shortcode_atts( [
+    'name'  => '',
+    'depth' => 0,
+    'class' => '',
+  ], $atts, 'wp_menu' );
+
+  if ( empty( $atts['name'] ) ) {
+    return '';
+  }
+
+  return wp_nav_menu( [
+    'theme_location'  => $atts['name'],
+    'depth'           => (int) $atts['depth'],
+    'menu_class'      => $atts['class'] ?: 'menu',
+    'container'       => false,
+    'echo'            => false,
+    'fallback_cb'     => false,
+  ] );
+}
+add_shortcode( 'wp_menu', 'seod_shortcode_wp_menu' );
+
 /* Menu toogle element
 ================================================== */
 function add_expand_collapsable_placeholder( $items, $args ) {
